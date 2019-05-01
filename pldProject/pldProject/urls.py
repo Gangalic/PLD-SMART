dc745023fdc8c5d8a7bda7f2a76ce42d1f818299
unknown <perez.benjamin@protonmail.com>
2019-05-01 12:20:57 +0200

    renaming

--- pldProject/pldProject/urls.py
+++ pldProject/pldProject/urls.py
@@ -1,4 +1,4 @@
-"""LyonQuest URL Configuration
+"""pldProject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
 urlpatterns = [
-    path('parcours/', include('parcours.urls')),
+    path('LyonQuest/', include('LyonQuest.urls')),
     path('admin/', admin.site.urls),
 ]
--- pldProject/pldProject/wsgi.py
+++ pldProject/pldProject/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for LyonQuest project.
+WSGI config for pldProject project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LyonQuest.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pldProject.settings')
 
 application = get_wsgi_application()
