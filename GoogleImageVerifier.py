533451347f169df7f95f93ba2a3152a64d25c0a3
Taki Eddine Mekhalfa <ft_mekhalfa@esi.dz>
2019-05-07 11:04:52 +0200

    Update GoogleImageVerifier.py

--- GoogleImageVerifier.py
+++ GoogleImageVerifier.py
@@ -19,25 +19,23 @@ class GoogleImageVerifier:
         response = self.client.label_detection(image=image)
         labels = response.label_annotations
         for label in labels:
-            # print(f'{label.description} : {label.score}')
+            print(f'{label.description} : {label.score}')
         for label in labels:
             if (label.description == targetLabel and label.score > 0.55):
                 return True 
         return False
         
 
+"""
+verifier = GoogleImageVerifier('creds.json')
+# The name of the image file to annotate
+file_name = os.path.join(
+    os.path.dirname(__file__),
+    '86843854_o.jpg')
 
-# ## Use example
-# verifier = GoogleImageVerifier('creds.json')
-
-# # The name of the image file to annotate
-# file_name = os.path.join(
-#     os.path.dirname(__file__),
-#     '86843854_o.jpg')
-
-# # Loads the image into memory and verify it against a label
-# with io.open(file_name, 'rb') as image_file:
-#     content = image_file.read()
-#     validImage = verifier.verify(content,"Stone carving")
-#     print(validImage)
-
+# Loads the image into memory and verify it against a label
+with io.open(file_name, 'rb') as image_file:
+    content = image_file.read()
+    validImage = verifier.verify(content,"Stone carving")
+    print(validImage)
+"""
