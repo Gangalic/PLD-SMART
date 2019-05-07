e831fba64e8b7666cb17c240bc2fc1ed6f9fd978
momeg <mohammed.megzari@insa-lyon.fr>
2019-05-07 16:11:06 +0200

    add getLabels to google image verifier

--- GoogleImageVerifier.py
+++ GoogleImageVerifier.py
@@ -1,7 +1,7 @@
 import io
 import os
-
-# Imports the Google Cloud client library
+import requests
+import json
 from google.cloud import vision
 from google.cloud.vision import types
 
@@ -19,25 +19,45 @@ class GoogleImageVerifier:
         response = self.client.label_detection(image=image)
         labels = response.label_annotations
         for label in labels:
-            # print(f'{label.description} : {label.score}')
+            print(f'{label.description} : {label.score}')
         for label in labels:
             if (label.description == targetLabel and label.score > 0.55):
                 return True 
         return False
-        
 
+    def getLabels(self,searchTerm):
+        imageUrl = getImagesUrl(searchTerm,1)[0]
+        response = self.client.label_detection({
+         'source': {'image_uri': imageUrl}
+        }, max_results= 5)
+        annotations = response.label_annotations
+        labels=[]
+        for annotation in annotations:
+          labels.append(annotation.description)
+        return labels
+
+def getImagesUrl(searchTerm, numResults):
+    params={
+        'q': searchTerm,
+        'cx': '011687418734408437358:mbfpvhsp4bm',
+        'searchType': 'image',
+        'num': numResults,
+        'key': os.environ['CUSTOM_SEARCH_API_KEY']
+    }
+    headers = {
+        'Accept': 'application/json'
+    }
+    url = 'https://www.googleapis.com/customsearch/v1'
+    r = requests.get(url, params=params, headers=headers)
+    json_data = json.loads(r.text)['items']
+    links = []
+    for item in json_data:
+        links.append(item['link'])
+    return links
 
 # ## Use example
 # verifier = GoogleImageVerifier('creds.json')
+# verifier.getLabels('arbre')#get available labels for a term
+# verifier.verify(content,"Stone carving") #verify an image against a target label
 
-# # The name of the image file to annotate
-# file_name = os.path.join(
-#     os.path.dirname(__file__),
-#     '86843854_o.jpg')
-
-# # Loads the image into memory and verify it against a label
-# with io.open(file_name, 'rb') as image_file:
-#     content = image_file.read()
-#     validImage = verifier.verify(content,"Stone carving")
-#     print(validImage)
 
